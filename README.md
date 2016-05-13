be610e2f1e0b9ccd9e5e60fc1493f342bdd0f44d
Noah Huppert <noahuppert@gmail.com>
2016-05-13 18:40:14 -0400

    added .dotrc capability

---
+++ README.md
@@ -0,0 +1,23 @@
+# .dotfiles
+A collection of my dotfiles
+
+# Setup
+1. Fish
+	- `sudo apt-get install fish -y`
+2. Fisherman
+	- `curl -Lo ~/.config/fish/functions/fisher.fish --create-dirs git.io/fisherman`
+3. Homeshick
+	- `git clone git://github.com/andsens/homeshick.git $HOME/.homesick/repos/homeshick`
+4. NVM
+	- `git clone git://github.com/creationix/nvm.git $HOME/.nvm` 
+5. Go
+	- `sudo add-apt-repository ppa:ubuntu-lxc/lxd-stable`
+	- `sudo apt-get update`
+	- `sudo apt-get install golang -y`
+6. `.dotrc`
+	- `dotrc-edit`
+		- This command will only work if you reloaded your shell after you cloned down this repository
+		- This is a result of the `dotrc-edit` function being declared in the Fish shell init file
+
+# `.dotrc
+The purpose of this file is to store configuration values which may differ between computers.
--- home/.config/fish/config.fish
+++ home/.config/fish/config.fish
@@ -1,4 +1,28 @@
 # Config
+# ---- .dotrc
+export DOTFILES_REPO_PATH="$HOME/.homesick/repos/.dotfiles"
+export DOTRC_FILE_PATH="$DOTFILES_REPO_PATH/.dotrc"
+
+function dotrc-edit
+	if test -e $DOTRC_FILE_PATH
+		eval $EDITOR $DOTRC_FILE_PATH
+	else
+		mv $DOTFILES_REPO_PATH/.dotrc.example $DOTFILES_REPO_PATH/.dotrc
+		eval $EDITOR $DOTRC_FILE_PATH
+		. "$HOME/.config/fish/config.fish"	
+	end
+end
+
+if test -e $DOTRC_FILE_PATH
+	. $DOTRC_FILE_PATH
+else
+	echo "[WARNING] .dotrc file not found, run `dotrc-edit` (looking in: $DOTFILES_REPO_PATH)"
+	echo "          config.fish will not load\n"
+	exit -1
+end
+
+
+
 # ---- Environment
 export EDITOR=vim
 
@@ -23,9 +47,13 @@ homeshick refresh --quiet
 
 # Languages
 # ---- NVM
-function nvm
-	bass source ~/.nvm/nvm.sh --no-use ';' nvm $argv
+if eval $DOTRC_NVM = true
+	function nvm
+		bass source ~/.nvm/nvm.sh --no-use ';' nvm $argv
+	end
 end
 
 # ---- Go
-export GOPATH="$HOME/Documents/workspace/go"
+if eval $DOTRC_GO = true
+	export GOPATH=$DOTRC_GOPATH
+end
